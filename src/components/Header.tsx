3e56e85ac0a20eb70620be5272703555c90bcf7e
ThanhLuan <thanhluanwebdesign@gmail.com>
2025-07-11 23:41:04 +0700

    moi ne

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { FiSearch, FiHeart } from "react-icons/fi";
 import ThemeToggle from "./ThemeToggle";
+import Link from "next/link";
 
 interface HeaderProps {
   searchTerm: string;
@@ -11,7 +12,7 @@ interface HeaderProps {
 export default function Header({ searchTerm, setSearchTerm }: HeaderProps) {
   return (
     <header className="flex flex-col md:flex-row items-center justify-between gap-4 p-6 bg-white dark:bg-neutral-800 shadow-md">
-      <div className="text-xl font-bold text-blue-700 dark:text-white">🎓 EduMall</div>
+      <div className="text-2xl font-bold text-blue-700 dark:text-white">🎓 EduMall</div>
 
       <div className="flex items-center w-full md:w-1/2 relative">
         <FiSearch className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 " />
@@ -20,16 +21,17 @@ export default function Header({ searchTerm, setSearchTerm }: HeaderProps) {
           placeholder="Tìm khoá học..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className="w-full pl-10 pr-4 py-2 rounded-md border border-gray-300 dark:border-gray-700 bg-white dark:bg-neutral-800 text-sm text-gray-700 dark:text-white"
+          className="w-full pl-10 pr-4 py-2 rounded-md border border-gray-300 dark:border-gray-700 bg-white dark:bg-neutral-800 text-sm c"
         />
       </div>
 
       <div className="flex items-center gap-4">
         <ThemeToggle />
-        <div className="flex items-center gap-1 cursor-pointer text-sm text-gray-600 hover:text-red-500">
-          <FiHeart className="text-lg" />
-          Favorites
-        </div>
+        <Link href="/favorites">
+  <div className="text-sm font-medium cursor-pointer text-gray-700 dark:text-white hover:text-blue-600 transition">
+    ❤️ Yêu thích
+  </div>
+</Link>
       </div>
     </header>
   );
